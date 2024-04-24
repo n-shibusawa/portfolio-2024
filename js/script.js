a0d6fb4e8091919bf69f8977d913423a87f2ea3e
n-shibusawa <n-shibusawa067@jutjoy.com>
2024-04-24 18:10:38 +0900

    work画面遷移・JSの追加・修正

--- js/script.js
+++ js/script.js
@@ -1,18 +1,50 @@
+'use script';
 // opening
 
 // cursorStoker
-const stalker = document.getElementsByClassName('parts-img_girl');
+const stalker = document.querySelector('.parts-img_girl');
+const stalkerImg = stalker.getElementsByTagName('img');
+let stalkerDisplay = true;
 document.addEventListener('mousemove', function (e) {
-    stalker[0].style.right = 'calc(100vw + -' + e.clientX + 'px - 5%)';
+    if(stalkerDisplay == true) {
+        let mouseX   = e.clientX,
+	    windowX = window.innerWidth;
+		direction = (windowX / 2) < mouseX ? 'right' : 'left';
+        if(direction == 'left') {
+            stalkerImg[0].classList.remove('right');
+        } else {
+            stalkerImg[0].classList.add('right');
+        }
+        stalker.style.right = 'calc(100vw + -' + mouseX + 'px - 5%)';
+    }
 });
 
-// work画面移行
-const workBtn = document.getElementsByClassName('nav_menu_work');
-const container = document.getElementsByClassName('container');
+// home→work画面移行
+const workBtn = document.querySelector('.nav_menu_work');
+const container = document.querySelector('.container');
+const nav = document.querySelector('.nav');
 function workClick() {
-    stalker[0].style.right = 'calc(120vw)';
+    stalkerDisplay = false;
+    nav.classList.add('none');
+    stalker.style.right = 'calc(120vw)';
     setTimeout(() => {
-        container[0].style.right = '-100vw';
-        stalker[0].style.display = 'none';
+        container.classList.add('work-area');
+        stalker.style.display = 'none';
     }, "2000");
+}
+
+// work→home画面移行
+const backWorkBtn = document.querySelector('.back_work');
+
+function backWorkClick() {
+    container.classList.remove('work-area');
+    setTimeout(() => {
+        stalker.style.display = 'block';
+        stalkerImg[0].classList.add('right');
+    }, "3000");
+    setTimeout(() => {
+        stalker.style.right = 'calc(45vw)';
+        nav.classList.remove('none');
+        stalkerDisplay = true;
+    }, "5000");
 }
