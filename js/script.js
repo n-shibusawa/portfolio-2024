8105bf9d8d292f3227d83d998ac98d28820245de
n-shibusawa <n-shibusawa067@jutjoy.com>
2024-04-25 18:54:37 +0900

    work画像追加、時間画像切り替え設定

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
 'use script';
 // opening
 
+// 空画像変更
+const nowTime = new Date().getHours();
+const sky = document.querySelector('.bg-img_sky');
+if(nowTime > 18 || nowTime < 5) {
+    sky.classList.add('night');
+}else if(nowTime > 16) {
+    sky.classList.add('evening');
+}else if(nowTime >= 5) {
+    sky.classList.add('morning');
+}
+
+// キャラ画像変更
+const girl = document.querySelector('.parts-img_girl img');
+if(nowTime > 20 || nowTime < 6) {
+    girl.src = 'img/parts_girl-sleep.png';
+}else {
+    girl.src = 'img/parts_girl-popular.png';
+}
+
 // cursorStoker
 const stalker = document.querySelector('.parts-img_girl');
 const stalkerImg = stalker.getElementsByTagName('img');
